dd4ab821cd62fd5d5de6d3aa1e4d5bf032ce6cb4
William Kewell <will_kewell@btinternet.com>
2026-03-04 21:59:56 +0000

    fix flipped mobile service, font size changes

--- src/services/Service.tsx
+++ src/services/Service.tsx
@@ -113,7 +113,7 @@ const Service = ({
                             marginRight: "12rem",
                           },
                           "@media (max-width: 600px)": {
-                            fontSize: "1.1rem",
+                            fontSize: "1rem",
                             marginRight: 0,
                           },
                         }}
@@ -208,7 +208,7 @@ const Service = ({
                     position: "absolute",
                     top: "-4rem",
                     right: "-2rem",
-                    marginLeft: "0",
+                    marginRight: "0",
                   },
                   "@media (max-width: 600px)": {
                     height: "10rem",
@@ -289,7 +289,7 @@ const Service = ({
                             marginRight: "12rem",
                           },
                           "@media (max-width: 600px)": {
-                            fontSize: "1.1rem",
+                            fontSize: "1rem",
                             marginRight: 0,
                           },
                         }}
@@ -319,78 +319,6 @@ const Service = ({
                 </Stack>
               </Box>
             </Box>
-            {/* <Grid container size={{ xs: 12 }} sx={{ height: "100%" }}>
-              <Grid size={{ xs: 5 }}>
-                <Box
-                  sx={{
-                    height: "100%",
-                    backgroundImage: `url(${image})`,
-                    backgroundRepeat: "no-repeat",
-                    backgroundSize: "cover",
-                    borderRadius: "2rem",
-                    marginRight: "2rem",
-                  }}
-                />
-              </Grid>
-              <Grid size={{ xs: 7 }}>
-                <Stack sx={{ height: "100%" }}>
-                  {intro && (
-                    <Typography
-                      variant={"h3"}
-                      sx={{ height: "10%", color: "white" }}
-                    >
-                      My Services
-                    </Typography>
-                  )}
-                  <Box
-                    sx={{
-                      height: intro ? "90%" : "100%",
-                      bgcolor: "#77566a",
-                      borderRadius: "2rem",
-                      padding: "2rem 1rem 2rem 1rem",
-                      color: "white",
-                    }}
-                  >
-                    <Stack
-                      height="100%"
-                      display="flex"
-                      flexDirection="column"
-                      justifyContent="space-evenly"
-                    >
-                      <Typography
-                        fontFamily="Questrial"
-                        variant="h4"
-                        sx={{ marginBottom: "1rem" }}
-                      >
-                        {title}
-                      </Typography>
-                      <Typography
-                        fontFamily="Questrial"
-                        sx={{
-                          marginBottom: "1rem",
-                          fontSize: "1.3cqw",
-                        }}
-                      >
-                        {text}
-                      </Typography>
-                      {bullets && <ServiceBullets bullets={bullets} />}
-                      {price.map((p) => (
-                        <Typography
-                          fontFamily="Cormorant Garamond"
-                          fontWeight={700}
-                          sx={{
-                            marginBottom: "1rem",
-                            fontSize: "1.4cqw",
-                          }}
-                        >
-                          {p}
-                        </Typography>
-                      ))}
-                    </Stack>
-                  </Box>
-                </Stack>
-              </Grid>
-            </Grid> */}
           </Box>
         </>
       )}
